d9fcc958f42751faa41b06950ddba76acb9e3fe0
Han Ruipeng <1102113739@qq.com>
2019-05-10 20:23:16 +0800

    韩模块

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div class="bigbox">
         <!-- 轮播 -->
         <van-swipe :autoplay="3000" indicator-color="#aaa" :height=200>
-            <van-swipe-item>1</van-swipe-item>
+            <van-swipe-item>2</van-swipe-item>
             <van-swipe-item>2</van-swipe-item>
             <van-swipe-item>3</van-swipe-item>
             <van-swipe-item>4</van-swipe-item>
